db2b356ffc20a1e17e8630960edbc19452030246
Manjunath-Dev <manjunathpv.dev@gmail.com>
2016-10-12 08:13:59 +0530

    1

---
+++ First.php
@@ -0,0 +1,130 @@
+ <section class="services high-margin" data-scroll-index="1">
+        <header class="section-header">
+            <div class="container">
+                <div class="row">
+                    <h1 class="section-title">About <span>TaiyoZen</span></h1>
+                    <div class="section-border"></div>
+                    <p>In Japanese script Taiyo mean <strong>“Sun”</strong> and Zen means <strong>“Energy”</strong> and together they spell <strong>“Sun-Energy”</strong>. This word showed the commitment & determination of the founding members towards the future.
+Team TAIYO ZEN has twenty five engineers with  the goal to  build and  race  a solar powered electric car. The  team  has  been designing    three wheeled solar powered vehicle with innovative  ideas  that  tackle  the challenge  of  designing  a  fully optimized  and  usable  vehicle  for today’s    market. For the upcoming year we plan to design & fabricate a Solar Powered Passenger vehicle which not nly helps in keeping the environment safe, it also the common man a cost saving alternative.    As    solar technology continues to be refined and advanced; we hope that platforms such as the Taiyo-Zen vehicle can be  used as  benchmarks  for  furthering design and innovation.
+</p></div>
+<h1 class="section-title">Our<span> Legacy</span></h1>
+ <div class="section-border"></div>
+                </div>
+            </div>
+        </header>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-3">
+                    <div class="block-container animateIn" data-animate="fadeInLeft">
+                        <div class="block-icon">
+                            <div class="block-triangle">
+                                <div data-angle="SE" class="flat-icon">
+                                    <i class="fa fa-newspaper-o"></i>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="block-title">
+                            <p>Year: 2014 - Our Beginning</p>
+                        </div>
+                        <div class="block-content">
+                            <p>TaiyoZen Team was founded in the year <strong>2014</strong>.
+                            <br>Participated & won the first <strong>ESVC championship</strong>.</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-md-3">
+                    <div class="block-container animateIn" data-animate="fadeInUp">
+                        <div class="block-icon">
+                            <div class="block-triangle">
+                                <div data-angle="SE" class="flat-icon">
+                                    <i class="fa fa-desktop"></i>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="block-title">
+                            <p>Year : 2015</p>
+                        </div>
+                        <div class="block-content">
+                            <p>Won the <strong>Indi-Asian Solar Challenge</strong> championship.
+                    		<br><strong>Best Innovaion Award</strong> in ESVC.<br>
+                    		Best Acceleration at Indo-Asian Solar Challenge.
+                    		<br> <strong>Team Lead:</strong> Sonu Singh</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-md-3">
+                    <div class="block-container animateIn" data-animate="fadeInUp">
+                        <div class="block-icon">
+                            <div class="block-triangle">
+                                <div data-angle="SE" class="flat-icon">
+                                    <i class="fa fa-thumbs-o-up"></i>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="block-title">
+                            <p>Year : 2016</p>
+                        </div>
+                        <div class="block-content">
+                            <p>Won the championship for ELECTRA by SAE India.
+                   				<br>Won the ESVC Championship
+                  				 <br>Won the Best Endurance award ESVC
+                  				 <br>Runners up at Indo-Asian Solar Challenge
+                  				 <br>Runners up-Endurance at Indo-Asian Solar Challenge.
+                  				 <br>Invited for Global Exhibition Services(Noida) for green mobility 2016.
+								<strong><br> Team Lead </strong>: Ajeesh Varghese
+.</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-md-3">
+                    <div class="block-container animateIn" data-animate="fadeInRight">
+                        <div class="block-icon">
+                            <div class="block-triangle">
+                                <div data-angle="SE" class="flat-icon">
+                                    <i class="fa fa-building-o"></i>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="block-title">
+                            <p>App Development</p>
+                        </div>
+                        <div class="block-content">
+                            <p>Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="spacer"></div>
+
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6 text-right animateIn" data-animate="bounceInRight">
+                    <h2 class="right-border">Want to say or find <br>something <span class="theme-color">else</span>?</h2>
+                    <p>TREND has everything you need to build an awesome website for<br> you or your company, and the possibilities are just endlress. Let <br>TREND upgrade your business</p>
+                    <a href="blog.html" class="outline-button button mt-blue">Read More</a>
+                </div>
+                <div class="col-md-6 text-center animateIn" data-animate="bounceInLeft">
+                    <img src="images/imac.png" alt="">
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="video-background high-margin">
+        <header class="section-header">
+            <div class="container">
+                <div class="row">
+                    <h1 class="section-title mt-c-white">Choosing the Right Direction</h1>
+                    <div class="section-border"></div>
+                    <p class="section-subtitle">Watch our latest introduction video to get our secret product information</p>
+                </div>
+            </div>
+        </header>
+        <div class='video-bg'></div>
+        <a class="player" data-property="{videoURL:'Scxs7L0vhZ4',containment:'.video-bg',autoPlay:true, mute:true, startAt:6,opacity:1,ratio:'16/9',showControls:false,showYTLogo:false,quality:'hd1080'}"></a>
+    </section>
