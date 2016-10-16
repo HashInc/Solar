454d4d6e3a437690075fea6f62f7b0eb3f991b4e
bluephantom180 <sriramakrishna13@gmail.com>
2016-10-16 17:09:25 +0530

    km

--- Third.php
+++ Third.php
@@ -6,7 +6,7 @@
                     <p> Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typeset.</p>
                 </div>
                 <div class="col-md-8 text-center animateIn" data-animate="bounceInRight">
-                    <img src="images/iPad.png" class="" alt="">
+                    <img src="images/work.jpg" class="" alt="">
                 </div>
             </div>
         </div>
